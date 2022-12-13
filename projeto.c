eb22307b92c3218ceecd23b86731ff29c7fbcf92
Miguel Cardoso <mi_lmc@hotmail.com>
2022-12-13 19:24:47 -0300

    entrega

--- projeto.c
+++ projeto.c
@@ -11,6 +11,7 @@ typedef struct Vinho {
 typedef struct Node {
   Vinho vinho;
   struct Node *next;
+  struct Node *previous;
 } Node;
 
 typedef struct estoque {
@@ -32,10 +33,11 @@ void Order(Estoque *estoque) {
   }
 }
 
-void AddVine(Vinho novoVinho, Estoque *estoque) {
+void AddWine(Vinho novoVinho, Estoque *estoque) {
   // primeira insercao
   if (estoque->qty == 0) {
     Node *primeiroVinho = malloc(sizeof(Node));
+    primeiroVinho->previous = NULL;
     primeiroVinho->vinho = novoVinho;
     estoque->primeiro = primeiroVinho;
     estoque->ultimo = primeiroVinho;
@@ -44,13 +46,14 @@ void AddVine(Vinho novoVinho, Estoque *estoque) {
     Node *newNode = malloc(sizeof(Node));
     newNode->vinho = novoVinho;
     newNode->next = NULL;
+    newNode->previous = estoque->ultimo;
     estoque->ultimo->next = newNode;
     estoque->ultimo = newNode;
     estoque->qty++;
   }
 }
 
-void saveVine(Estoque *estoque, Estoque *estoqueOrdenado) {
+void saveWine(Estoque *estoque, Estoque *estoqueOrdenado) {
   Vinho novoVinho;
   char isEspecial;
   printf("Digite o nome do vinho: ");
@@ -66,16 +69,21 @@ void saveVine(Estoque *estoque, Estoque *estoqueOrdenado) {
   __fpurge(stdin);
   scanf("%d", &novoVinho.safra);
   if (isEspecial == 's') {
-    AddVine(novoVinho, estoqueOrdenado);
+    AddWine(novoVinho, estoqueOrdenado);
     Order(estoqueOrdenado);
   } else {
-    AddVine(novoVinho, estoque);
+    AddWine(novoVinho, estoque);
   }
 }
 
 void popWine(Estoque *estoque) {
+  if(estoque->qty == 0){
+    printf("Estoque Vazio!!!!\n");
+    return;
+  }
   Node *primeiroVinho = estoque->primeiro;
   estoque->primeiro = primeiroVinho->next;
+  estoque->qty--;
 
   printf("=========================\n");
   printf("nome do vinho: %s\n", primeiroVinho->vinho.nome);
@@ -85,40 +93,31 @@ void popWine(Estoque *estoque) {
   printf("=========================\n");
 }
 
-void Print(Estoque *estoque) {
-  Node *x;
-  for (x = estoque->primeiro; x != NULL; x = x->next) {
-    printf("=========================\n");
-    printf("nome do vinho: %s\n", x->vinho.nome);
-    printf("pais de origem: %s\n", x->vinho.pais);
-    printf("tipo de uva: %s\n", x->vinho.tipoUva);
-    printf("safra: %d\n", x->vinho.safra);
-    printf("=========================\n");
-  }
-}
-
 void Print5Last(Estoque *estoque, Estoque *estoqueOrdenado) {
-  Estoque newAdega;
-  newAdega.qty = 0;
+  Estoque newEstoque;
+  newEstoque.qty = 0;
   Node *current = estoque->primeiro;
   while (current != NULL) {
-    AddVine(current->vinho, &newAdega);
-    Order(&newAdega);
+    AddWine(current->vinho, &newEstoque);
+    Order(&newEstoque);
     current = current->next;
   }
 
-  current = estoque->primeiro;
+  current = estoqueOrdenado->primeiro;
   while (current != NULL) {
-    AddVine(current->vinho, &newAdega);
-    Order(&newAdega);
+    AddWine(current->vinho, &newEstoque);
+    Order(&newEstoque);
     current = current->next;
   }
-  current = newAdega.primeiro;
+  current = newEstoque.primeiro;
+
   for (int i = 0; i < 5; i++) {
+    if (current == NULL)
+      break;
     printf("=========================\n");
-    printf("nome do vinho: %s\n", current->vinho.nome);
-    printf("pais de origem: %s\n", current->vinho.pais);
-    printf("tipo de uva: %s\n", current->vinho.tipoUva);
+    printf("nome: %s\n", current->vinho.nome);
+    printf("pais: %s\n", current->vinho.pais);
+    printf("tipo da uva: %s\n", current->vinho.tipoUva);
     printf("safra: %d\n", current->vinho.safra);
     printf("=========================\n");
     current = current->next;
@@ -126,80 +125,69 @@ void Print5Last(Estoque *estoque, Estoque *estoqueOrdenado) {
 }
 
 void Print5Lasts(Estoque *estoque, Estoque *estoqueOrdenado) {
-  Estoque newAdega;
-  newAdega.qty = 0;
+  Estoque newEstoque;
+  newEstoque.qty = 0;
   Node *current = estoque->primeiro;
   while (current != NULL) {
-    AddVine(current->vinho, &newAdega);
-    Order(&newAdega);
+    AddWine(current->vinho, &newEstoque);
+    Order(&newEstoque);
     current = current->next;
   }
 
   current = estoqueOrdenado->primeiro;
   while (current != NULL) {
-    AddVine(current->vinho, &newAdega);
-    Order(&newAdega);
+    AddWine(current->vinho, &newEstoque);
+    Order(&newEstoque);
     current = current->next;
   }
-  current = newAdega.primeiro;
-  int size = newAdega.qty;
 
-  for (int i = 0; i < size - 5; i++)
-    current = current->next;
-
+  current = newEstoque.ultimo;
   for (int i = 0; i < 5; i++) {
+    if (current == NULL)
+      break;
+    
     printf("=========================\n");
-    printf("nome do vinho: %s\n", current->vinho.nome);
-    printf("pais de origem: %s\n", current->vinho.pais);
+    printf("nome: %s\n", current->vinho.nome);
+    printf("pais: %s\n", current->vinho.pais);
     printf("tipo de uva: %s\n", current->vinho.tipoUva);
     printf("safra: %d\n", current->vinho.safra);
     printf("=========================\n");
-    current = current->next;
+    current = current->previous;
   }
 }
-
 int main(void) {
   __fpurge(stdin);
-  Estoque Adega, AdegaOrdenada;
+  Estoque Estoque, EstoqueOrdenado;
   int opt;
-  Adega.qty = 0;
-  AdegaOrdenada.qty = 0;
+  Estoque.qty = 0;
+  EstoqueOrdenado.qty = 0;
 
   while (1) {
     printf("1 - Adicionar um vinho\n");
     printf("2 - Retirar vinho s/ ocasiao especial\n");
-    printf("3 - Consultar todos os vinhos s/ ocasiao especial\n");
-    printf("4 - Consultar todos os vinhos para ocasioes especiais\n");
-    printf("5 - Mostrar e retirar vinho p/ ocasioes especiais com safra mais "
+    printf("3 - Retirar vinho p/ ocasioes especiais com safra mais "
            "recente \n");
-    printf("6 - Mostrar 5 aquisições mais antigas\n");
-    printf("7 - Mostrar 5 aquisições mais recentes\n");
+    printf("4 - Mostrar 5 aquisições mais antigas\n");
+    printf("5 - Mostrar 5 aquisições mais recentes\n");
     scanf("%d", &opt);
     __fpurge(stdin);
     switch (opt) {
     case 1:
-      saveVine(&Adega, &AdegaOrdenada);
+      saveWine(&Estoque, &EstoqueOrdenado);
       break;
 
     case 2:
-      popWine(&Adega);
+      popWine(&Estoque);
       break;
 
     case 3:
-      Print(&Adega);
+      popWine(&EstoqueOrdenado);
       break;
-
     case 4:
-      Print(&AdegaOrdenada);
+      Print5Last(&Estoque, &EstoqueOrdenado);
       break;
     case 5:
-      popWine(&AdegaOrdenada);
-      break;
-    case 6:
-      Print5Last(&Adega, &AdegaOrdenada);
-      break;
-    case 7:
-      Print5Lasts(&Adega, &AdegaOrdenada);
+      Print5Lasts(&Estoque, &EstoqueOrdenado);
       break;
     }
   }
